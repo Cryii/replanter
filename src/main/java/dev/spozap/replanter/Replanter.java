b4e42715cc42738230f70a8461bcca5a78e4a81b
spozap <spozapdev@gmail.com>
2024-02-17 21:31:52 +0100

    initial commit

---
+++ src/main/java/dev/spozap/replanter/Replanter.java
@@ -0,0 +1,17 @@
+package dev.spozap.replanter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Replanter extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
